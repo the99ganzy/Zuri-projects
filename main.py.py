8ea294ca02a97b0f99d426d661db477095c757bf
the99ganzy <99679708+the99ganzy@users.noreply.github.com>
2022-05-29 01:09:46 +0200

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,16 @@
+
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+
+    while len(word) == len(anagram):
+    # [assignment] Add your code here
+        if sorted('peach') == sorted('cheap'):
+            return True
+        return False
+
+print(find_anagram('cheap', 'peach'))
